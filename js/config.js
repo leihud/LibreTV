7c8a0608e6e5e321e061195ce9b64911e3c8c4fe
leihud <804632879@11.com>
2026-05-26 15:58:50 +0800

    Merge pull request #867 from bestZwei/main
    修改网站标题

--- js/config.js
+++ js/config.js
@@ -13,9 +13,9 @@ const PASSWORD_CONFIG = {
 
 // 网站信息配置
 const SITE_CONFIG = {
-    name: 'LibreTV',
+    name: '蚕宝宝TV',
     url: 'https://libretv.is-an.org',
-    description: '免费在线视频搜索与观看平台',
+    description: '蚕宝宝的小电视',
     logo: 'image/logo.png',
     version: '1.0.3'
 };
